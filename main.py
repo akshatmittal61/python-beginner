4413e789e8ddc62e51adb69c9bdbe703a8576eb9
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-05 11:04:15 +0530

    arithmetic operations

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # print('Hello World')
-#
 # name = input('Please enter your name: ')
 # print(f'Hi, {name} \nI am Gojo')
-#
 # x = 7 + 61
 # print(x)
 
-first_name = 'akshat'
-last_name = 'mittal'
-
+# first_name = 'akshat'
+# last_name = 'mittal'
 # print('Hello! My name is ' + first_name.capitalize() + ' ' + last_name.capitalize())
 # print('Hello! My name is {} {}'.format(first_name, last_name))
 # print('Hello! My name is {1} {0}'.format(last_name, first_name))
-print(f'Hello! My name is {first_name} {last_name}')
+# print(f'Hello! My name is {first_name} {last_name}')
+
+age = 7
+# Error cannot concatenate int and str
+# print('Jake is ' + age + 'years old')
+# n1 = input('Number 1: ')  # 3
+# n2 = input('Number 2: ')  # 2
+# print(n1 + n2)  # Prints string concatenation -> '4' + '2' = '32'
+n1 = int(input('Number 1: '))  # 3
+n2 = int(input('Number 2: '))  # 2
+print(n1 + n2)  # Prints addition -> 3 + 2 = 5
+print(n1 - n2)  # Subtraction -> 3 - 2 = 1
+print(n1 * n2)  # Multiplication -> 3 * 2 = 6
+print(n1 / n2)  # Division -> 3 / 2 = 1.5
+print(n1 ** n2)  # Exponent -> 3 ** 2 = 9
