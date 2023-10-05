09dc9eda097050f140b4de500a6d473b86f30cfe
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-05 10:57:39 +0530

    formatting string

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-print('Hello World')
+# print('Hello World')
+#
+# name = input('Please enter your name: ')
+# print(f'Hi, {name} \nI am Gojo')
+#
+# x = 7 + 61
+# print(x)
 
-name = input('Please enter your name: ')
-print(f'Hi, {name} \nI am Gojo')
+first_name = 'akshat'
+last_name = 'mittal'
 
-x = 7 + 61
-print(x)
+# print('Hello! My name is ' + first_name.capitalize() + ' ' + last_name.capitalize())
+# print('Hello! My name is {} {}'.format(first_name, last_name))
+# print('Hello! My name is {1} {0}'.format(last_name, first_name))
+print(f'Hello! My name is {first_name} {last_name}')
